47d0d8e61e58815ad31b3ed1d89b7e360ec201ea
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2022-05-30 14:30:36 +0300

    codecommit roles

--- backend/variables.tf
+++ backend/variables.tf
@@ -2,3 +2,6 @@ variable "aws_region" {}
 variable "tfstate_bucket" {}
 variable "dynamo_db_table" {}
 variable "log_bucket" {}
+
+variable "codebuild_iam_role" {}
+variable "codebuild_iam_role_policy" {}
--- codecommit/outputs.tf
+++ codecommit/outputs.tf
@@ -1,6 +1,7 @@
 output "codecommit_repo_arn" {
   value = aws_codecommit_repository.repository.arn
 }
+
 output "codecommit_repo_name" {
   value = aws_codecommit_repository.repository.repository_name
 }
