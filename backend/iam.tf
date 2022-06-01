5e023b6625ccac2830b59541890c37ffa17d5d04
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2022-06-01 09:37:52 +0300

    add privilege

--- backend/iam.tf
+++ backend/iam.tf
@@ -89,8 +89,8 @@ resource "aws_iam_role_policy" "codebuild_iam_role_policy" {
     },
     {
       "Effect": "Allow",
-      "Action": "ec2:*",
-      "Resource": "${aws_iam_role.codebuild_iam_role.arn}"
+      "Action": "*",
+      "Resource": "*"
     }
   ]
 }
